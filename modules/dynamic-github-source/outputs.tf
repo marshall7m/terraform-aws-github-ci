af1fcfdfbc2b7aa82af49acf251c556a99818967
marshall7m <44485531+marshall7m@users.noreply.github.com>
2021-05-19 09:47:23 -0700

    fix gh-request-validator output name

--- modules/dynamic-github-source/outputs.tf
+++ modules/dynamic-github-source/outputs.tf
@@ -1,9 +1,9 @@
 output "api_invoke_url" {
-  value = module.github_webhook.invoke_url
+  value = module.github_webhook_request_validator.invoke_url
 }
 
 output "request_validator_function_arn" {
-  value = module.github_webhook.function_arn
+  value = module.github_webhook_request_validator.function_arn
 }
 
 output "payload_validator_function_arn" {
