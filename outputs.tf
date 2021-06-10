14161f6803459112e9debf9c90b38f9dc23232e4
marshall7m <44485531+marshall7m@users.noreply.github.com>
2021-06-10 14:30:25 -0700

    fix ssm out

--- outputs.tf
+++ outputs.tf
@@ -31,5 +31,5 @@ output "lambda_deps" {
 
 output "github_token_ssm_arn" {
   description = "ARN of the AWS System Manager Parameter Store key used for the sensitive GitHub Token"
-  value       = var.create_github_token_ssm_param == true ? aws_ssm_parameter.github_token[0].arn : data.aws_ssm_parameter.github_token[0].arn
+  value       = try(aws_ssm_parameter.github_token[0].arn, data.aws_ssm_parameter.github_token[0])
 }
