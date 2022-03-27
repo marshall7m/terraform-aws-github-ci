50b100ad14271f09e583c567e263a4113baec980
marshall7m <44485531+marshall7m@users.noreply.github.com>
2022-03-27 10:29:23 -0700

    fix filter group type and revise var description

--- variables.tf
+++ variables.tf
@@ -34,25 +34,34 @@ variable "repos" {
 List of named repos to create github webhooks for and their respective filter groups
 Params:
   `name`: Repository name
-  `filter_groups`: {
-    `events` - List of Github Webhook events that will invoke the API. Currently only supports: `push` and `pull_request`.
-    `pr_actions` - List of pull request actions (e.g. opened, edited, reopened, closed). See more under the action key at: https://docs.github.com/en/developers/webhooks-and-events/webhook-events-and-payloads#pull_request
-    `base_refs` - List of base refs
-    `head_refs` - List of head refs
-    `actor_account_ids` - List of Github user IDs
-    `commit_messages` - List of commit messages
-    `file_paths` - List of file paths
-    `exclude_matched_filter` - If set to true, labels filter group as invalid if it is matched
-  }
+  `filter_groups`: List of filter groups that the Github event has to meet. The event has to meet all filters of atleast one group in order to succeed. 
+  [
+    [ (Filter Group)
+      {
+        `type`: The type of filter
+          (
+            `events` - List of Github Webhook events that will invoke the API. Currently only supports: `push` and `pull_request`.
+            `pr_actions` - List of pull request actions (e.g. opened, edited, reopened, closed). See more under the action key at: https://docs.github.com/en/developers/webhooks-and-events/webhook-events-and-payloads#pull_request
+            `base_refs` - List of base refs
+            `head_refs` - List of head refs
+            `actor_account_ids` - List of Github user IDs
+            `commit_messages` - List of commit messages
+            `file_paths` - List of file paths
+          )
+        `pattern`: Regex pattern that is searched for within the related event attribute
+        `exclude_matched_filter` - If set to true, labels filter group as invalid if it is matched
+      }
+    ]
+  ]
   EOF
-  type = list(list(object({
+  type = list(object({
     name = string
-    filter_groups = optional(list(object({
+    filter_groups = optional(list(list(object({
       type                   = string
       pattern                = string
       exclude_matched_filter = optional(bool)
-    })))
-  })))
+    }))))
+  }))
   default = []
 }
 
