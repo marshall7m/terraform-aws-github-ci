cb657c8788cd4e93335c8a48004c52ec9541644b
marshall7m <44485531+marshall7m@users.noreply.github.com>
2022-05-19 16:32:00 -0700

    rm docker testing env cfg and use gh container registry image

--- setup.py
+++ setup.py
@@ -1,10 +1,6 @@
 from setuptools import setup, find_packages
 
-install_requires = [
-    'pytest-terra-fixt @ git+https://github.com/marshall7m/pytest-terra-fixt@v0.3.3#egg=pytest-terra-fixt'
-]
 setup(
     name='terraform-aws-github-webhook',
-    install_requires=install_requires,
     packages=find_packages()
 )
